2444cdaaa4b1eeb1db22dcfe2f478d90c9369041
Leonardo Maldonado <lmaldonadomar@outlook.com>
2025-08-08 15:19:17 -0500

    Feat: Introduce TabView structure to ImageGalleryApp

--- Use Cases/ImageGallery/ImageGallery/Application/ImageGalleryApp.swift
+++ Use Cases/ImageGallery/ImageGallery/Application/ImageGalleryApp.swift
@@ -13,12 +13,38 @@ struct ImageGalleryApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                ImageGalleryView(coordinator: coordinator)
-                    .ignoresSafeArea(.all)
-                    .navigationDestination(item: $coordinator.currentRoute) { route in
-                        ImageGalleryRouter.destination(for: route)
-                    }
+            TabView {
+                NavigationStack {
+                    ImageGalleryView(coordinator: coordinator)
+                        .ignoresSafeArea(.all)
+                        .navigationDestination(item: $coordinator.currentRoute) { route in
+                            ImageGalleryRouter.destination(for: route)
+                        }
+                }.tabItem {
+                    Image(systemName: "photo")
+                    Text("Photos")
+                }
+                
+                NavigationStack {
+                    Text("Memories")
+                }.tabItem {
+                    Image(systemName: "square.stack")
+                    Text("Memory")
+                }
+                
+                NavigationStack {
+                    Text("Library")
+                }.tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Library")
+                }
+                
+                NavigationStack {
+                    Text("Search")
+                }.tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
             }
         }
     }
